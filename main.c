377d3f3b86982ae3a5d1233ce26eae4340dea4f8
Austin <phanuphongkuharungkitcharoen@gmail.com>
2025-09-24 17:36:48 +0700

    function1-saverecord

--- main.c
+++ main.c
@@ -1,2 +1,10 @@
 #include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
 
+// บันทึกข้อมูล 
+void saverecordCSV(const char *EquipmentName,int EquipmentID,const char *Status,int MaintenanceData){
+    FILE *fp = fopen("equipment.csv","a");
+
+    
+}
---
+++ test1 saverecord/saverecordCSV-test.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+// บันทึกข้อมูล 
+void saverecordCSV(const char *EquipmentName,const char *EquipmentID,const char *Status,const char *MaintenanceData)
+{
+    FILE *fp = fopen("equipment-test.csv","a");
+    if(fp == NULL)
+    {
+        printf("can't open file %s \n","equipment-test.csv");
+        return ;
+    }
+    else 
+    {
+        fprintf(fp ,"%s,%s,%s,%s \n",EquipmentName,EquipmentID,Status,MaintenanceData);
+        fclose(fp);
+    }
+}
+
+int main()
+{
+    char filename[]= "equipment-test.csv" ;
+    char EquipmentName[50] ;
+    char EquipmentID[6];
+    char Status[6];
+    char MaintenanceData[12];
+
+    printf("** Enter information **\n");
+    
+    //EquipmentName input
+    printf("EquipmentName :");
+    scanf("%s",EquipmentName);
+
+    //EquipmentID input
+    printf("EquipmentID :");
+    scanf("%s",EquipmentID);
+
+    //EquipmentID input
+    printf("Status :");
+    scanf("%s",Status);
+
+    //EquipmentID input
+    printf("MaintenanceData :");
+    scanf("%s",MaintenanceData);
+
+    saverecordCSV(EquipmentName,EquipmentID,Status,MaintenanceData);
+    printf("Finish");
+    return 0;
+}
