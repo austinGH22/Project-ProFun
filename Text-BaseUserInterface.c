8f28f1da8e622488aacf5d67f124e3442adaaaab
Austin <phanuphongkuharungkitcharoen@gmail.com>
2025-09-24 18:59:50 +0700

    Text-BaseUserInterface

---
+++ Text-BaseUserInterface.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+
+int main(){
+    
+    int choice;
+    //compusive only 1-5 choice 
+    do {
+            printf("##### Factory Equipment Operation Management System #####\n");
+    printf("Menu\n");
+    printf("1)Search\n");
+    printf("2)Add\n");
+    printf("3)update\n");
+    printf("4)Delete\n");
+    printf("5)Exit Program\n");
+
+    
+    printf("Choose function (1-5):");
+    scanf("%d",&choice);
+    } while (choice != 1 && choice !=2 && choice !=3 && choice !=4 && choice !=5 );
+
+    switch (choice)
+    {
+    case 1:
+        
+        break;
+    case 2:
+        
+        break;
+    case 3:
+        
+        break;
+    case 4:
+        
+        break;
+    case 5:
+        
+        break;
+    default:
+        break;
+    }
+    
+    return 0;
+}
