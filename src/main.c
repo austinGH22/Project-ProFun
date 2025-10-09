02ca2d24beb2a32013f2c1dd7f2138a4be6c226c
AustinGH22 <phanuphongkuharungkitcharoen@gmail.com>
2025-10-09 20:16:00 +0700

    mainisgood unittestis alittle for success , all e2e

--- src/main.c
+++ src/main.c
@@ -3,81 +3,122 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <assert.h>
 
-int main() {
-    Machine *list=NULL;
-    int count=0;
+int main()
+{
+    Machine *list = NULL;
+    int count = 0;
     int choice;
     char input[100];
-    int c ;
+    int c;
 
     count = readCSVToArray("data.csv", &list);
-    while(1){
+    while (1)
+    {
         printf("\n======== Factory Equipment Management ========\n");
-        printf("1) Read CSV\n2) Add Machine\n3) Search Machine\n4) Update Machine\n5) Delete Machine\n6) Exit\n");
-        printf("Select an option (1-6) : ");
-        
+        printf("1) Read CSV\n2) Add Machine\n3) Search Machine\n4) Update Machine\n5) Delete Machine\n6) Exit\n7) Run Unit Tests\n");
+        printf("Select an option (1-8) : ");
 
         fgets(input, sizeof(input), stdin);
         input[strcspn(input, "\n")] = 0;
         int isNumber = 1;
-        for (int i = 0; i < strlen(input); i++) {
-            if (!isdigit((unsigned char)input[i])) {
+        for (int i = 0; i < strlen(input); i++)
+        {
+            if (!isdigit((unsigned char)input[i]))
+            {
                 isNumber = 0;
                 break;
             }
         }
-        if (!isNumber) {
-            printf("\n ! You can choose only integer(1-6)\n");
+        if (!isNumber)
+        {
+            printf("\n ! You can choose only integer(1-8)\n");
             continue;
         }
         choice = atoi(input);
-        if (choice < 1 || choice > 6) {
-            printf("\n ! Only numbers 1-6 can be used.\n");
+        if (choice < 1 || choice > 8)
+        {
+            printf("\n ! Only numbers 1-8 can be used.\n");
             continue;
         }
 
-        
-        switch(choice){
-            case 1:
-                free(list); list=NULL;
-                count=readCSVToArray("data.csv",&list);
-                if(count>0){
-                    printf("\n--------------- Machine List ---------------\n");
-                    printf("NAME  |  CODE  |  STATUS  |  MAINTENANCE DATA\n \n");
-                    for(int i=0;i<count;i++)
-                        printf("%s | %s | %s | %s\n",
-                               list[i].name,list[i].code,list[i].status,list[i].maintenanceDate);
-                } else printf("No data found.\n");
-                printf("--------------------------------------------- \n");
+        switch (choice)
+        {
+        case 1:
+            free(list);
+            list = NULL;
+            count = readCSVToArray("data.csv", &list);
+            if (count > 0)
+            {
+                printf("\n--------------- Machine List ---------------\n");
+                printf("NAME  |  CODE  |  STATUS  |  MAINTENANCE DATA\n \n");
+                for (int i = 0; i < count; i++)
+                    printf("%s | %s | %s | %s\n",
+                           list[i].name, list[i].code, list[i].status, list[i].maintenanceDate);
+            }
+            else
+                printf("No data found.\n");
+            printf("--------------------------------------------- \n");
+            break;
+        case 2:
+            printf("--------------- ADD MENU ---------------\n");
+            addMachine("data.csv", &list, &count);
+            break;
+        case 3:
+            printf("search \"Name\" or \"Code\" or \"Status\" or \"Maintenance data\"for search: ");
+            char kw[MAX_FIELD];
+            if (!fgets(kw, MAX_FIELD, stdin))
+            {
+                continue;
+            }
+            kw[strcspn(kw, "\n")] = '\0';
+            trimNewline(kw);
+            if (strlen(kw) == 0)
+            {
+                printf("Input must not empty!");
+                continue;
+            }
+            searchMachine(list, count, kw);
+            break;
+        case 4:
+            if (count == 0)
+            {
+                printf("No data loaded. Read first.\n");
                 break;
-            case 2: 
-                printf("--------------- ADD MENU ---------------\n");
-                addMachine("data.csv",&list,&count); 
+            }
+            updateMachine("data.csv", list, count);
+            break;
+        case 5:
+            if (count == 0)
+            {
+                printf("No data loaded. Read first.\n");
                 break;
-            case 3:
-                printf("search \"Name\" or \"Code\" or \"Status\" or \"Maintenance data\"for search: "); 
-                char kw[MAX_FIELD];
-                if (!fgets(kw,MAX_FIELD,stdin)){
-                    continue;
-                }
-                kw[strcspn(kw, "\n")] = '\0';
-                trimNewline(kw);
-                if(strlen(kw) == 0){
-                    printf("Input must not empty!");
-                    continue;
-                }
-                searchMachine(list,count,kw);
-                break;
-            case 4:
-                if(count==0){ printf("No data loaded. Read first.\n"); break;}
-                updateMachine("data.csv",list,count); break;
-            case 5:
-                if(count==0){ printf("No data loaded. Read first.\n"); break;}
-                deleteRecordCSV("data.csv",&list,&count); break;
-            case 6:
-                free(list); printf("Exiting program...\n"); return 0;
-            default: printf(" Invalid choice.\n");
+            }
+            deleteRecordCSV("data.csv", &list, &count);
+            break;
+        case 6:
+            free(list);
+            printf("Exiting program...\n");
+            return 0;
+        case 7:
+            printf("===== UNIT TEST START =====\n");
+            test_readCSVToArray_normal();
+            test_addMachine_normal();
+            // Boundary Tests
+            test_readCSV_emptyFile();
+            test_addMachine_firstEntry();
+            // Extreme Tests
+            test_addMachine_longInput();
+            test_readCSV_invalidFormat();
+            // Memory Safety Test
+            test_memoryLeaks();
+            printf("\n===== ALL TESTS PASSED SUCCESSFULLY =====\n\n");
+        case 8:
+            //e2e_test();
+            break;
+        default:
+            printf(" Invalid choice.\n");
         }
     }
 
--- src/project.h
+++ src/project.h
@@ -4,6 +4,9 @@
 #define MAX_LINE 256
 #define MAX_FIELD 50
 
+#define TEST_FILE "test_data.csv"
+#define MOCK_INPUT "mock_input.txt"
+
 typedef struct {
     char name[MAX_FIELD];
     char code[MAX_FIELD];
@@ -19,5 +22,18 @@ void searchMachine(Machine *list, int count, char *keyword);
 void updateMachine(const char *filename, Machine *list, int count);
 void deleteRecordCSV(const char *filename, Machine **list, int *count);
 
+//unit test 
+void create_test_csv();
+void clear_csv();
+void test_readCSVToArray_normal();
+void test_addMachine_normal();
+void test_readCSV_emptyFile();
+void test_addMachine_firstEntry();
+void test_addMachine_longInput();
+void test_readCSV_invalidFormat();
+void test_memoryLeaks();
+
+//E2E test
+void e2e_test();
 #endif 
 
